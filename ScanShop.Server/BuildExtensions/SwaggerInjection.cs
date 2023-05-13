100558514528e001e5df0e7ebe207dc388ec5225
Wednumi <mykhailo.sereda256@gmail.com>
2023-05-13 10:55:38 +0300

    setup swagger

---
+++ ScanShop.Server/BuildExtensions/SwaggerInjection.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.OpenApi.Models;
+
+namespace ScanShop.Server.BuildExtensions
+{
+    internal static class SwaggerInjection
+    {
+        internal static void AddSetSwagger(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(setup =>
+            {
+                setup.SwaggerDoc(
+                    "v1",
+                    new OpenApiInfo
+                    {
+                        Title = "ScanShop Api",
+                        Version = "v1",
+                    });
+                setup.AddSecurityDefinition(
+                    "Bearer",
+                    new OpenApiSecurityScheme
+                    {
+                        In = ParameterLocation.Header,
+                        Description = "Please enter a valid token",
+                        Name = "Authorization",
+                        Type = SecuritySchemeType.Http,
+                        BearerFormat = "JWT",
+                        Scheme = "Bearer"
+                    });
+                setup.AddSecurityRequirement(
+                    new OpenApiSecurityRequirement
+                    {
+                        {
+                            new OpenApiSecurityScheme
+                            {
+                                Reference = new OpenApiReference
+                                {
+                                    Type=ReferenceType.SecurityScheme,
+                                    Id="Bearer"
+                                }
+                            },
+                            new string[]{}
+                        }
+                    });
+                setup.EnableAnnotations();
+            });
+        }
+    }
+}
--- ScanShop.Server/Program.cs
+++ ScanShop.Server/Program.cs
@@ -1,15 +1,13 @@
+using ScanShop.Server.BuildExtensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSetSwagger();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
