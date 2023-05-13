7fcd50920d12fb2cf247083ba891c7ccc6b04b65
Wednumi <mykhailo.sereda256@gmail.com>
2023-05-13 11:52:15 +0300

    setup database

---
+++ ScanShop.Db/DbContext/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ScanShop.Db.DbContext
+{
+    public class ApplicationDbContext : IdentityDbContext<IdentityUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            :base(options) { }
+    }
+}
--- ScanShop.Server/Program.cs
+++ ScanShop.Server/Program.cs
@@ -1,10 +1,12 @@
 using ScanShop.Server.BuildExtensions;
+using ScanShop.Db;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSetSwagger();
+builder.Services.AddDbSetup(builder.Configuration);
 
 var app = builder.Build();
 
