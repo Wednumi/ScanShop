59096efdd2003f7beb45a662903981a8b95ca5e6
Wednumi <mykhailo.sereda256@gmail.com>
2023-05-14 17:40:16 +0300

    Added entities

---
+++ ScanShop.Db/Entities/Category.cs
@@ -0,0 +1,8 @@
+﻿namespace ScanShop.Db.Entities
+{
+    public class Category : BaseEntity
+    {
+        public string Title { get; set; }
+        public IList<Product> Products { get; set; }
+    }
+}
---
+++ ScanShop.Db/Entities/Review.cs
@@ -0,0 +1,11 @@
+﻿namespace ScanShop.Db.Entities
+{
+    public class Review : BaseEntity
+    {
+        public Guid UserId { get; set; }
+        public ShopUser? User { get; set; }
+        public string Text { get; set; }
+        public DateTime Date { get; set; }
+        public int Rating { get; set; }
+    }
+}
