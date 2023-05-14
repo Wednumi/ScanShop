09a81d87cac5afe1b9120c87c222eaf7c85ec808
Wednumi <mykhailo.sereda256@gmail.com>
2023-05-14 20:07:10 +0300

    Registered Identity to DI

--- ScanShop.Server/Features/Account/SignUpCommand.cs
+++ ScanShop.Server/Features/Account/SignUpCommand.cs
@@ -1,4 +1,5 @@
 ﻿using MediatR;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using ScanShop.Shared.Dto.Account;
 
@@ -9,6 +10,13 @@ namespace ScanShop.Server.Features.Account
         public class SignUpCommandHandler
             : IRequestHandler<SignUpCommand, ActionResult<SignUpResponse>>
         {
+            private readonly UserManager<IdentityUser> _userManager;
+
+            public SignUpCommandHandler(UserManager<IdentityUser> userManager)
+            {
+                _userManager = userManager;
+            }
+
             public async Task<ActionResult<SignUpResponse>> Handle(SignUpCommand request, CancellationToken cancellationToken)
             {
                 throw new NotImplementedException();
