153132e8a2d47cc41ca789ab65719e5d88bd41db
Artem Volovyi <artem.volovyi@nure.ua>
2023-05-22 13:09:29 +0300

    Mobile initial commit

---
+++ ScanShop.Mobile/ScanShop.Mobile/AppShell.xaml.cs
@@ -0,0 +1,19 @@
+﻿using ScanShop.Mobile.ViewModels;
+using ScanShop.Mobile.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace ScanShop.Mobile
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+    }
+}
---
+++ ScanShop.Mobile/ScanShop.Mobile/Views/AboutPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ScanShop.Mobile.Views
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
