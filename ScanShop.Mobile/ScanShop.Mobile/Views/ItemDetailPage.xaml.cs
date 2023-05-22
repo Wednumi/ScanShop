2a4efdc5ba0c26a4753b93df58fbd6d2df66e571
Artem Volovyi <artem.volovyi@nure.ua>
2023-05-22 13:09:29 +0300

    Initial commit

---
+++ ScanShop.Mobile/ScanShop.Mobile/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using ScanShop.Mobile.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace ScanShop.Mobile.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
