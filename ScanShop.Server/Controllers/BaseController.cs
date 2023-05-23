9f31e97a57ae204ffe956077fa112a9be216b4cd
Mykhailo Sereda <mykhailo.sereda@cruxlab.com>
2023-05-23 23:27:23 +0300

    Extended result handling

--- ScanShop.Server/Controllers/BaseController.cs
+++ ScanShop.Server/Controllers/BaseController.cs
@@ -17,5 +17,18 @@ namespace ScanShop.Server.Controllers
             }
             return StatusCode(StatusCodes.Status500InternalServerError, result);
         }
+
+        protected ActionResult FromFeatureResult<T>(FeatureResult<T> result) where T : class 
+        {
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            if (result.UserErrors is not null && result.UserErrors.Any())
+            {
+                return BadRequest(result);
+            }
+            return StatusCode(StatusCodes.Status500InternalServerError, result);
+        }
     }
 }
--- ScanShop.Shared/Result/FeatureResult.cs
+++ ScanShop.Shared/Result/FeatureResult.cs
@@ -20,6 +20,26 @@ namespace ScanShop.Shared.Result
             ServerErrors = serverErrors;
             UserErrors = userErrors;
         }
+
+        public static FeatureResult FromUserError(IError error)
+        {
+            return new FeatureResult(userErrors: new List<IError>() { error });
+        }
+
+        public static FeatureResult FromServerError(IError error)
+        {
+            return new FeatureResult(serverErrors: new List<IError>() { error });
+        }
+
+        public static FeatureResult<T> FromUserError<T>(IError error) where T : class
+        {
+            return new FeatureResult<T>(userErrors: new List<IError>() { error });
+        }
+
+        public static FeatureResult<T> FromServerError<T>(IError error) where T : class
+        {
+            return new FeatureResult<T>(serverErrors: new List<IError>() { error });
+        }
     }
 
     public class FeatureResult<T> : FeatureResult where T : class
@@ -34,5 +54,9 @@ namespace ScanShop.Shared.Result
 
         public FeatureResult(List<IError> userErrors = null, List<IError> serverErrors = null)
             :base(userErrors, serverErrors) { }
+
+        public FeatureResult(FeatureResult featureResult)
+            :base(featureResult.UserErrors, featureResult.ServerErrors)
+        { }
     }
 }
