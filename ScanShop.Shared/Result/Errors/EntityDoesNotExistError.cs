844f82a12be802e2420ad26111eaf32e92c04ad5
Mykhailo Sereda <mykhailo.sereda@cruxlab.com>
2023-05-28 16:10:06 +0300

    Added  GetUserInfoQuery handling

---
+++ ScanShop.Shared/Result/Errors/EntityDoesNotExistError.cs
@@ -0,0 +1,7 @@
+﻿namespace ScanShop.Shared.Result.Errors
+{
+    public class EntityDoesNotExistError : IError
+    {
+        public string Message { get; set; } = "Entity with those parameters does not exist";
+    }
+}
---
+++ ScanShop.Shared/Result/Errors/MappingError.cs
@@ -0,0 +1,7 @@
+﻿namespace ScanShop.Shared.Result.Errors
+{
+    public class MappingError : IError
+    {
+        public string Message { get; set; } = "Error of mapping entity";
+    }
+}
