157ad3a0329d8ceef86a9bf6f8f4be1f45bd09be
Artem Volovyi <artem.volovyi@nure.ua>
2023-05-28 17:10:41 +0300

    Add mobile logout

---
+++ ScanShop.Mobile/ScanShop.Mobile/ViewModels/ProfileViewModel.cs
@@ -0,0 +1,21 @@
+﻿using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace ScanShop.Mobile.ViewModels
+{
+    public class ProfileViewModel : BaseViewModel
+    {
+        public Command LogoutCommand { get; }
+
+        public ProfileViewModel()
+        {
+            LogoutCommand = new Command(OnLogoutClicked);
+        }
+
+        private async void OnLogoutClicked(object obj)
+        {
+            SecureStorage.Remove("BearerToken");
+            await Shell.Current.GoToAsync("//LoginPage");
+        }
+    }
+}
