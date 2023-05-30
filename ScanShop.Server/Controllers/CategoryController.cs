9f98c3fb8245d7abf8db3aacacc15c742148002f
Mykhailo Sereda <mykhailo.sereda@cruxlab.com>
2023-05-30 23:29:45 +0300

    Added categories creade, update, read

---
+++ ScanShop.Server/Controllers/CategoryController.cs
@@ -0,0 +1,40 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ScanShop.Db.DbContext;
+using ScanShop.Db.Entities;
+using ScanShop.Shared.Dto;
+
+namespace ScanShop.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : BaseController
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly IMapper _mapper;
+
+        public CategoryController(ApplicationDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        [HttpPost("update")]
+        public async Task<ActionResult> UpdateCategory(CategoryDto categoryDto)
+        {
+            var category = _mapper.Map<Category>(categoryDto);
+            _context.Categories.Update(category);
+            await _context.SaveChangesAsync();
+            return Ok(category);
+        }
+
+        [HttpGet("all")]
+        public async Task<ActionResult> GetAll()
+        {
+            var categories = await _context.Categories.ToListAsync();
+            var dtos = _mapper.Map<List<Category>, List<CategoryDto>>(categories);
+            return Ok(dtos);
+        }
+    }
+}
---
+++ ScanShop.Shared/Dto/Product/ProductDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ScanShop.Shared.Dto.Product
+{
+    public class ProductDto
+    {
+        public string Title { get; set; }
+        public float Price { get; set; }
+        public float Discount { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+        public int Amount { get; set; }
+        public Guid CategoryId { get; set; }
+        public string CategoryName { get; set; }
+    }
+}
