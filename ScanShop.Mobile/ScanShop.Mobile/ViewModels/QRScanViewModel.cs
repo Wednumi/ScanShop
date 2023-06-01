8e7d6fac7e09ac76c7d1e7e158818a815b9be270
Коротун Дмитро <dmytro.korotun@nure.ua>
2023-06-01 14:57:51 +0200

    Added basic qr scan page

---
+++ ScanShop.Mobile/ScanShop.Mobile/ViewModels/QRScanViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using ZXing;
+
+namespace ScanShop.Mobile.ViewModels
+{
+    public class QRScanViewModel : BaseViewModel
+    {
+        public Command ScanCommand { get; set; }
+        private string _QRScanText = "Scan the QR code";
+        public QRScanViewModel()
+        {
+            ScanCommand = new Command(ScanResultAction);
+        }
+        public string QRScanText
+        {
+            get => _QRScanText;
+            set
+            {
+                if (value == _QRScanText)
+                    return;
+                _QRScanText = value;
+                OnPropertyChanged();
+            }
+        }
+        private void ScanResultAction(object obj)
+        {
+            var result = obj as ZXing.Result;
+            QRScanText = result?.Text ?? string.Empty;
+        }
+    }
+}
--- ScanShop.Mobile/ScanShop.Mobile/Views/QRScanPage.xaml.cs
+++ ScanShop.Mobile/ScanShop.Mobile/Views/QRScanPage.xaml.cs
@@ -1,15 +1,16 @@
-﻿using System;
-using System.ComponentModel;
+﻿using ScanShop.Mobile.ViewModels;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace ScanShop.Mobile.Views
 {
+    [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class QRScanPage : ContentPage
     {
         public QRScanPage()
         {
             InitializeComponent();
+            BindingContext = new QRScanViewModel();
         }
     }
 }
