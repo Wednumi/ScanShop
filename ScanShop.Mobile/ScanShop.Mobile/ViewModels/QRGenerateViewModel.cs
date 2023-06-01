5d5f51ac9bf55934c61b89ad0649f8f9d2498e15
Коротун Дмитро <dmytro.korotun@nure.ua>
2023-06-01 20:37:51 +0200

    Added QR Generation Page

---
+++ ScanShop.Mobile/ScanShop.Mobile/ViewModels/QRGenerateViewModel.cs
@@ -0,0 +1,52 @@
+﻿using ScanShop.Mobile.Views;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace ScanShop.Mobile.ViewModels
+{
+    public class QRGenerateViewModel : BaseViewModel
+    {
+        private string _QRGenValue = "Hello World!";
+        private int _QRSize = Convert.ToInt32(Math.Round(DeviceDisplay.MainDisplayInfo.Width / DeviceDisplay.MainDisplayInfo.Density * QRSizePercentage / 100));
+        private const float QRSizePercentage = 80;
+
+        public Command GoBackCommand { get; }
+
+        public QRGenerateViewModel()
+        {
+            GoBackCommand = new Command(OnGoBackClicked);
+        }
+
+        public string QRGenValue
+        {
+            get => _QRGenValue;
+            set
+            {
+                if (value == _QRGenValue)
+                    return;
+                _QRGenValue = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public int QRSize
+        {
+            get => _QRSize;
+            set
+            {
+                if (value == _QRSize)
+                    return;
+                _QRSize = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private async void OnGoBackClicked()
+        {
+            await Shell.Current.GoToAsync("//ProfilePage");
+        }
+    }
+}
