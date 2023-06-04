babc33ba475ed4509e3e1779207d5dc16e873051
grimerssy <grimerssy@gmail.com>
2023-06-04 11:21:48 +0200

    fix logo import

--- client/src/components/Header/index.tsx
+++ client/src/components/Header/index.tsx
@@ -1,5 +1,5 @@
 import { getCart } from "@api";
-import Logo from "@assets/Logo.png";
+import Logo from "@assets/logo.png";
 import CategoriesButton from "./CategoriesButton";
 import SearchField from "./SearchField";
 import FavouriteButton from "./FavouriteButton";
